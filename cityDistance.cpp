2c3891fa2a969fe504c8e4faf8307a06243a4fa5
Nicholas Vaughan <nick@HAL2000.Nicks_domain>
2016-03-19 00:52:31 -0500

    final edition to cityDistance.cpp

--- cityDistance.cpp
+++ cityDistance.cpp
@@ -6,23 +6,60 @@
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
+
+
+//converts degrees into radians
+double torad(double d) {
+   double r = (d * M_PI)/180.0;
+   return r;
+}
+
+//calculates distance
+double haversine(double lat1, double lat2, double lon1, double lon2) {
+
+   const double EARTH_RADIUS = 6372797.56;  //In units of meters
+   double rlat1 = torad(lat1);
+   double rlat2 = torad(lat2);
+   double rlon1 = torad(lon1);
+   double rlon2 = torad(lon2);
+
+   double dlat = rlat2 - rlat1;
+   double dlon = rlon2 - rlon1;
+   
+   double a = pow(sin(dlat/2), 2) + cos(rlat1) *
+              cos(rlat2) * pow(sin(dlon/2), 2);
+   double b = 2 * asin(sqrt(a));
+
+   return EARTH_RADIUS * b;
+
+}
 
 int main() {
    
    ifstream input("./statecity.csv");
-   ofstream output("stateCityOutput.txt");
+   ofstream output("cityDistanceOutput.txt");
   
    if(!input.is_open()) {
       cerr << "Cannot open file\n";
       abort();
    }
-   string STATE, state, city, north, south, east, west;
-   double latitude, longitude;
-   int city_count=0;
-   cout << "Input the two letter state code(e.g.'CO'):\n";
-   cin  >> STATE;
 
+   string STATE1, STATE2, CITY1, CITY2, state, city;
+   double latitude, longitude, lat1, lat2, lon1, lon2, dist, midist, kmdist;
+
+   //Prompts for input
+   cout << "Input name of first city, then the two letter state code,\n"
+           "seperated by comma, no space after comma (e.g. Longmont,CO):\n";
+   getline(cin, CITY1, ',');
+   getline(cin, STATE1);
+
+   cout << "Input name of second city, then the two letter state code,\n"
+           "seperated by comma (e.g. Houston,TX):\n";
+   getline(cin, CITY2, ',');
+   getline(cin, STATE2);
+   
    input.ignore(200, '\n');  //skip first line
 
    while(input.good()) {
@@ -37,23 +74,27 @@ int main() {
       input >> longitude;
       input.ignore(8,'\n');
       
-      if (state==STATE) {
-         ++city_count;
-         north = maxLat(latitude, city);
-         south =minLat(latitude, city);
-         east = maxLong(longitude, city);
-         west = minLong(longitude, city);
+      if(state == STATE1 && city == CITY1) {
+         lat1 = latitude;
+         lon1 = longitude;
+      }
+
+      if(state == STATE2 && city == CITY2) {
+         lat2 = latitude;
+         lon2 = longitude;
       }
    }
 
-   output << "Programming Assignment #4.\n";
+   dist = haversine(lat1, lat2, lon1, lon2);
+   midist = dist/1609.34;
+   kmdist = dist/1000.0;
+
+   //Output formatting
+   output << "Programming Assignment #5.\n";
    output << "Nicholas Vaughan\n";
-   output << "March 10, 2016\n" << endl;
-   output << "The state of " << STATE << " has " << city_count << " cities.\n" << endl;
-   output << north << " is the Northernmost city.\n";
-   output << south << " is the Southernmost city.\n";
-   output << east << " is the Easternmost city.\n";
-   output << west << " is the Westernmost city.\n";
+   output << "March 18, 2016\n" << endl;
+   output << "The distance from "<<CITY1<<", "<<STATE1<<" to "<<CITY2
+          <<", "<<STATE2<<" is \n"<<midist<<" miles ("<<kmdist<<" km)";
 
    return 0;
    
